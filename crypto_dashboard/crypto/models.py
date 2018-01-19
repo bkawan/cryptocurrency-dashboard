e5764533ce71ce8631c5b53624e72be79fc624ed
bkawan <bikeshkawang@gmail.com>
2018-01-19 22:08:14 +0545

    create crytp app and some models

---
+++ crypto_dashboard/crypto/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+CURRENCY_CHOICES = (
+    ('XRP', 'XRP'),
+    ('BTC', 'BTC'),
+    ('LTC', 'LTC'),
+)
+BOT_CHOICES = (
+    ('BOT1', 'BOT1'),
+    ('BOT2', 'BOT2'),
+    ('BOT3', 'BOT3'),
+)
+
+
+class CryptoCurrency(models.Model):
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=10)
+    bot = models.CharField(choices=BOT_CHOICES, max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    profit = models.FloatField()
+    balance = models.FloatField()
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.bot, self.CURRENCY_CHOICES, self.created_at)
+
+
+class LastTrade(models.Model):
+    info = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=255)
+    volume = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.info, self.quantity, self.value)
