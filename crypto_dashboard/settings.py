4522ca6de086bf8e7f6453d1ddb632a67121852c
bkawan <bikeshkawang@gmail.com>
2018-01-23 22:22:26 +0545

    filter with select bot

--- crypto_dashboard/settings.py
+++ crypto_dashboard/settings.py
@@ -121,8 +121,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_FILTER_BACKENDS':('django_filters.rest_framework.DjangoFilterBackend',),
-    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE':100
+    # 'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE':100
 
 }
 STATIC_URL = '/static/'
