e31a35bc0513edaf94a27abe626f929983018061
bkawan <bikeshkawang@gmail.com>
2018-01-27 10:56:10 +0545

    add  hourly filtering

--- apps/crypto/admin.py
+++ apps/crypto/admin.py
@@ -2,5 +2,8 @@
 from  apps.crypto.models import CryptoCurrency, LastTrade
 from django.contrib import admin
 
-admin.site.register(CryptoCurrency)
+
+class CryptoCurrencyAdmin(admin.ModelAdmin):
+    list_display = ['bot','currency','created_at']
+admin.site.register(CryptoCurrency,CryptoCurrencyAdmin)
 admin.site.register(LastTrade)
--- apps/crypto/api/filters.py
+++ apps/crypto/api/filters.py
@@ -1,18 +1,26 @@
-from django_filters import DateFromToRangeFilter
+from datetime import timedelta
+from django.utils.datetime_safe import datetime
+from django_filters import DateFromToRangeFilter, ChoiceFilter, DateTimeFromToRangeFilter
 from django_filters.rest_framework import FilterSet
 
 from apps.crypto.models import CryptoCurrency
 
 
 class CryptoCurrencyFilter(FilterSet):
-    created_at = DateFromToRangeFilter(name='created_at')
+    # HOURLY_CHOICES = (
+    #     ('last', 'Last Hour'),
+    # )
+    created_at = DateTimeFromToRangeFilter(name='created_at')
+    # hourly = ChoiceFilter(choices=HOURLY_CHOICES,name='created_at', method='get_hourly')
 
     class Meta:
         model = CryptoCurrency
         fields = ['bot',
                   'created_at',
+                  # 'hourly',
                   ]
 
-
-
-
+    # def get_hourly(self, queryset, name, value):
+    #     if value == 'last':
+    #         return queryset.filter(created_at__range=((datetime.now()-timedelta(hours=1)),datetime.now()))
+    #     return queryset
